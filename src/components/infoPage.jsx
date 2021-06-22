73b633a305c44b3c7fa53d61da1182e4041ae045
Mezeru <jermpro0@gmail.com>
2021-06-23 01:36:54 +0530

    Socket fetch workign

--- src/components/infoPage.jsx
+++ src/components/infoPage.jsx
@@ -1,4 +1,4 @@
-import React, { useEffect } from "react";
+import React, { useState } from "react";
 import ReactDOM from "react-dom";
 import { Link } from "react-router-dom";
 import 'regenerator-runtime/runtime';
@@ -8,30 +8,44 @@ import socketIOClient from 'socket.io-client';
 
 export default function infoPage(){
 
+  let history = useHistory();
+
+  const [alerts,useAlerts] = useState([]);
+  const [disable,useDisable] = useState();
+
   const handleClick = (e) => {
     e.preventDefault();
-    socket.emit('fetchData',"Badlapur");
-    
+    let token;
+    socket.emit('fetchData',"Badlapur",token);
   }
   
-    const socket = socketIOClient("http://localhost:3001/");
-    socket.on('connnect', () =>{
-      console.log("Retriving data");
-    })
-
+    const socket = socketIOClient("http://localhost:3001/",);
     socket.on('sendAlerts', (alerts) =>{
+      useAlerts(alerts);
+      useDisable("none");
       console.log(alerts);
-    })
+    });
     
-
-  let history = useHistory();
+    const alertCards = alerts.map(alert => {
+      return(
+        <div className="alert-card">
+          <h1>{alert.contactinfo}</h1>
+          <p>{alert.Time}</p>
+        </div>
+      )
+    })
 
     return(
       <>
-      <div className="container">
-        <button onClick={handleClick}>Send Request</button>
+      <div className="alerts">
+        {alertCards}
       </div>
 
+      <div style={{display:disable}} className="home-btns">
+        <button className="hm-btn" onClick={handleClick}>Get Alerts</button>
+      </div>
+      
+     
       </>
     )
 
