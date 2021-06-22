c98c1652e8387659d6a5765eda1fddee0ec60b22
Mezeru <jermpro0@gmail.com>
2021-06-22 20:44:59 +0530

    gets alerts

--- src/components/infoPage.jsx
+++ src/components/infoPage.jsx
@@ -1,24 +1,39 @@
-import React from "react";
+import React, { useEffect } from "react";
 import ReactDOM from "react-dom";
 import { Link } from "react-router-dom";
 import 'regenerator-runtime/runtime';
 import logoutCall from '../scripts/logoutCall'
 import { useHistory } from "react-router";
+import socketIOClient from 'socket.io-client';
 
 export default function infoPage(){
 
+  const handleClick = (e) => {
+    e.preventDefault();
+    socket.emit('fetchData',"Badlapur");
+    
+  }
+  
+    const socket = socketIOClient("http://localhost:3001/");
+    socket.on('connnect', () =>{
+      console.log("Retriving data");
+    })
+
+    socket.on('sendAlerts', (alerts) =>{
+      console.log(alerts);
+    })
+    
+
   let history = useHistory();
 
-    const handleClick = async (e) => {
-        e.preventDefault();
-        // const resp = logoutCall(token);
-
-        history.push('/login');
-        
-    }
-
     return(
-      <button onClick={e => {handleClick(e)}} >LOGOUT</button>
+      <>
+      <div className="container">
+        <button onClick={handleClick}>Send Request</button>
+      </div>
+
+      </>
     )
 
 }
+
